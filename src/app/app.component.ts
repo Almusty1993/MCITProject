5a600ac9825678cbb17cd3832c91de74adaae123
Almusty1993 <alanamumusty@gmail.com>
2023-05-28 12:59:10 -0400

    getting The `AppComponent` class is a standalone component, which can not be used in the `@NgModule.bootstrap` array. Use the `bootstrapApplication` function for bootstrap instead.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+	imports: [NgbNavModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'clientSide';
+  active = 'top';
 }
+
+
